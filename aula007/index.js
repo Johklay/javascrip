83c8c28ea62441b4ee6ce9a3a8c0e8899d4d708f
Johklay <johnklayverton@gmail.com>
2025-11-26 19:44:38 -0300

    update

---
+++ aula007/index.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoQuintuplo = resultadoDuplicado * 5;
+resultadoQuintuplo = resultadoQuintuplo + 5
+console.log(resultadoQuintuplo);
+
+
+console.log(typeof primeiroNumero);
