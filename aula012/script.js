c5c4541e85fd81bfa8229983135bdc01a5c0c20b
Johklay <johnklayverton@gmail.com>
2025-12-04 20:10:30 -0300

    update

--- aula012/script.js
+++ aula012/script.js
@@ -1,1 +1,6 @@
-alert ('Minha mensagem');
+let num1 = prompt ('Digite um número');
+let num2 = prompt ('Digite outro número');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert('a soma dos valores foi de' + resultado);
