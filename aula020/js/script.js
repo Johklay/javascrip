620507f2b52d24c25c6651ff28fdc264c01a4a62
Johklay <johnklayverton@gmail.com>
2026-02-05 16:55:20 -0300

    update

---
+++ aula020/js/script.js
@@ -0,0 +1,36 @@
+function meuEscopo () {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector ('.resultado');
+
+    const pessoas = [];
+
+
+    function  recebeEventoForm (evento) {
+        evento.preventDefault();
+        
+        const nome = form.querySelector ('.nome')
+        const sobrenome = form.querySelector ('.sobrenome')
+        const peso = form.querySelector ('.peso')
+        const altura = form.querySelector ('.altura')
+
+        resultado.innerHTML = `Nome: ${nome.value} <br>
+        Sobrenome: ${sobrenome.value}<br>
+        Peso: ${peso.value}<br>
+        Altura: ${altura.value}`;
+         
+        const pessoa = {
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            nome: altura.value
+        };
+
+        pessoas.push(pessoa)
+
+        console.log(pessoas)
+       console.log(nome.value, sobrenome.value, peso.value, altura.value)
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
