2c72e215e553b094d2d55b42151862978272e5f9
Johklay <johnklayverton@gmail.com>
2026-02-06 16:21:52 -0300

    Create script.js

---
+++ revisoes/aula020/script.js
@@ -0,0 +1,10 @@
+console.log("Estudando de leve na sexta")
+
+let nome = 'John';
+let idade = 22;
+
+console.log(nome)
+console.log(idade);
+
+let jogo = 'Resident evil 4';
+console.log ('meu jogo favorito é ' + jogo)
