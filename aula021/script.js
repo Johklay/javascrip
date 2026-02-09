24bf12c221892a8e6f76922d2495b1fe9fc95c21
Johklay <johnklayverton@gmail.com>
2026-02-09 16:04:45 -0300

    create

---
+++ aula021/script.js
@@ -0,0 +1,22 @@
+/*
+Principais operadores de comparação
+
+Operador	Significado	                    Exemplo	           Resultado
+
+>	      Maior que	                        10 > 5	             true
+>=	      Maior ou igual	                10 >= 10	         true
+<	      Menor que	                        5 < 10	             true
+<=	      Menor ou igual	                5 <= 4	             false
+==	      Igual (valor)	                    5 == "5"	         true (Não utilizar esse operador)
+===	      Igual estrito (valor e tipo)	    5 === "5"	         false
+!=	      Diferente (valor)	                5 != "5"	         false (Não utilizar esse operador)
+!==	      Diferente estrito(valor e tipo)	5 !== "5"	         true
+
+*/
+
+/*const comp  = 10 > 5;
+console.log (comp)*/
+
+const comp  = 10 >= 5;
+console.log (comp);
+
---
+++ aula022/script.js
@@ -0,0 +1,53 @@
+/*
+Operadores Lógicos
+
+&& = AND = (E)
+
+|| = OR = (OU)
+
+! = NOT = (NÃO)
+
+*/
+
+/*
+TUDO PRECISA SER VERDADEIRO
+
+true && true   // true
+true && false  // false
+false && true  // false
+false && false // false
+
+let idade = 20;
+let temCarteira = true;
+
+idade >= 18 && temCarteira // true
+
+*/
+
+/*
+BASTA UM SER VERDADEIRO
+
+true || true   // true
+true || false  // true
+false || true  // true
+false || false // false
+
+let temDinheiro = false;
+let temPix = true;
+
+temDinheiro || temPix // true
+
+*/
+
+/*
+INVERTE O VALOR
+
+!true  // false
+!false // true
+
+let logado = false;
+
+!logado // true
+
+*/
+
