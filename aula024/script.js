ee61617ae19847c7396d75079b1090410c92b888
Johklay <johnklayverton@gmail.com>
2026-02-11 16:30:03 -0300

    create

---
+++ aula024/script.js
@@ -0,0 +1,34 @@
+/*
+Entre 0 - 11 - Bom dia
+Entre 12 - 17 - Boa tarde
+Entre 18 - 23 - boa noite
+*/
+
+//If pode ser usado sozinho
+//Sempre que utilizo a palavra else, preciso de un If antes
+//eu posso ter varios else ifs na checagem
+//so posso ter um else na checagem
+//Podemos usar condições sem else if utilizando apenas if e else
+// Else não se usa () parenteses ou seja, não tem condição
+
+//const hora = 50;
+
+//if (hora >= 0 && hora <= 11) {
+//    console.log('Bom dia');
+//} else if (hora >= 12 && hora <= 17) {
+//    console.log('Boa tarde');
+//} else if (hora >= 18 && hora <= 23) {
+//    console.log('Boa noite');
+//} else {
+//    console.log('Olá');
+//}
+
+
+const tenhoGrana = true;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('não vou sair de casa');
+}
+    
---
+++ revisoes/aula024/script.js
@@ -0,0 +1,9 @@
+const nota = 4;
+
+if(nota >=7){
+    console.log('Aprovado');
+} else if  (nota >= 5 && nota < 7){
+    console.log('recuperação')
+} else{
+    console.log('Reprovado')
+}
