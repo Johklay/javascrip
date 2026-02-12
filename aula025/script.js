73a3bd23fcceccdb620d245d85f806a7fc30e7e9
Johklay <johnklayverton@gmail.com>
2026-02-12 18:07:32 -0300

    update

---
+++ aula025/script.js
@@ -0,0 +1,10 @@
+const numero = 5;
+
+// Se (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
+// Se não faça isso {o código}
+
+if (numero >= 0 && numero <= 5) {
+    console.log (`Sim, o número ${numero} está entre 0 e 5`);
+} else {
+    console.log (`O número ${numero} NÃO está entre 0 e 5`)
+}
