56c2a3d3d91b82b1a6be11e6f0e668f52824264b
Johklay <johnklayverton@gmail.com>
2026-02-26 17:06:46 -0300

    criei exercicio

---
+++ aula027/script.js
@@ -0,0 +1,12 @@
+const pontuacaoUsuario = 999;
+const nivelusuario = pontuacaoUsuario >= 1000 ? 'usuario VIP' :'Usuario normal';
+console.log(nivelusuario)
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta'
+
+/*if (pontuacaoUsuario = 1000) {
+    console.log('Usuario VIP')
+} else {
+    console.log('Usuario normal')
+}*/
