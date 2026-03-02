be5d0aa12f4d69f0de1bad90e6e873473319bfd1
Johklay <johnklayverton@gmail.com>
2026-03-02 15:26:32 -0300

    Update script.js

--- aula028/script.js
+++ aula028/script.js
@@ -1,4 +1,4 @@
-const hoje = new Date();
+/*const hoje = new Date();
 const dia= hoje.getDate();
 const mes= hoje.getMonth() + 1;
 const ano= hoje.getFullYear();
@@ -7,4 +7,29 @@ const diaSemana= hoje.getDay();
 const horas= hoje.getHours();
 const minutos= hoje.getMinutes();
 
-console.log(`${dia}/${mes}/${ano} ${horas}:${minutos}  ${diaSemana}`)
+console.log(`${dia}/${mes}/${ano} ${horas}:${minutos} ${diaSemana}`)*/
+
+
+const hoje = new Date();
+const dataBrasil = formataData(hoje); 
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`
+}
+
+/**
+ * @param {Date} hoje
+ */
+function formataData(hoje) {
+    const dia=  zeroAEsquerda (hoje.getDate());
+    const mes=  zeroAEsquerda (hoje.getMonth() + 1);
+    const ano=  zeroAEsquerda (hoje.getFullYear());
+    const diaSemana=  zeroAEsquerda (hoje.getDay());
+
+    const horas= zeroAEsquerda (hoje.getHours());
+    const minutos= zeroAEsquerda (hoje.getMinutes());
+    const segundos= zeroAEsquerda (hoje.getSeconds());
+    
+    return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos} ${diaSemana}`
+}
+console.log(dataBrasil);
