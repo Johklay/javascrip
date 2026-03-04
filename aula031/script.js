be675397c16744448cb33b6896052c511fd3724d
Johklay <johnklayverton@gmail.com>
2026-03-04 16:32:55 -0300

    Create script.js

---
+++ aula031/script.js
@@ -0,0 +1,14 @@
+/*let a = 'A';
+let b ='B';
+let c = 'C';
+
+const letras = [b,c,a];
+[a, b, c] = letras;
+console.log(a, b, c);*/
+
+
+const numeros = [1000, 2000, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const[um, , tres, , cinco, , ,] = numeros;
+
+console.log(um, tres, cinco);
+//console.log(resto)
