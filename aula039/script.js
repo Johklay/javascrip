3b50d01076b065f735c31ff13a1b643dc3193325
Johklay <johnklayverton@gmail.com>
2026-03-17 16:52:01 -0300

    update

---
+++ aula039/script.js
@@ -0,0 +1,15 @@
+const x = 10;
+const y = 1;
+
+console.log(max(x,y))
+
+
+// function max (x, y){
+//     return x > y ? x : y;
+// }
+
+ function max (x, y)
+ { if (x > y){
+     return x 
+    }  else { return y } }
+
---
+++ aula040/script.js
@@ -0,0 +1,9 @@
+const largura = 1920;
+const altura = 1080;
+
+console.log(epaisagem(largura, altura))
+
+function epaisagem(largura, altura){
+    return largura > altura;
+    
+}
