d5d8459fd1fc2e6c0b2e8d14bfb1480e92a8dcdd
Johklay <johnklayverton@gmail.com>
2026-03-31 16:25:07 -0300

    create script

---
+++ aula048/script.js
@@ -0,0 +1,58 @@
+//ARGUMENTOS QUE SUSTENTA TODOS OS ARGUMENTOS ENVIADOS
+
+// function funcao() {
+// let total = 0;
+// for (let argumento of arguments) {
+//     total += argumento;
+// }
+
+// console.log(total);
+// }
+
+// funcao(1, 2, 3, 4, 5, 6, 7 );
+'----------------------------------------------------------------------------------------------'
+
+// function funcao(a, b, c, d, e, f) {
+//     console.log(a, b, c, d, e, f);
+// }
+
+// funcao(1, 2, 3);
+'-----------------------------------------------------------------------------------------------'
+
+// function funcao(a, b = 2, c = 4) {
+//     console.log(a + b + c)
+// }
+
+// funcao(2, undefined, 10);
+'------------------------------------------------------------------------------------------------'
+
+// function funcao([valor1, valor2, valor3]) {
+//     console.log(valor1, valor2, valor3);
+// }
+
+// funcao(['Luiz Otavaio', 'Miranda', 20])
+'-----------------------------------------------------------------------------------------------'
+
+// const conta = function conta(operador, acumulador, ...numeros) {
+//     for (let numero of numeros) {
+//         if (operador === '+') acumulador += numero;
+//         if (operador === '-') acumulador -= numero;
+//         if (operador === '/') acumulador /= numero;
+//         if (operador === '*') acumulador *= numero;
+
+//         console.log(acumulador);
+//     }
+// }
+// conta ('+', 1, 20, 30, 40, 50);
+'----------------------------------------------------------------------------------------------'
+
+// const conta = function conta(operador, acumulador, ...numeros) {
+//     console.log(operador, acumulador, numeros)
+// }
+// conta ('+', 1, 20, 30, 40, 50);
+'----------------------------------------------------------------------------------------------'
+
+const conta = function conta(...args) {
+    console.log(args);
+};
+conta ('+', 1, 20, 30, 40, 50);
