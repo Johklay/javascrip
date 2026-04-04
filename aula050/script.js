166a2f76ebdae11042c0b97e3280a30e1a10ac43
Johklay <johnklayverton@gmail.com>
2026-04-04 11:56:05 -0300

    update

---
+++ aula050/script.js
@@ -0,0 +1,14 @@
+//ESCOPO LEXICO
+
+const nome = "Luiz"
+
+function falaNome() {
+    const nome = 'Otavio';
+    console.log(nome)
+}
+
+function usaFalaNome() {
+    falaNome()
+}
+
+usaFalaNome()
