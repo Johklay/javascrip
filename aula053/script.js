f7b5f8c8950a890d2302b8071deac01b5c59a4df
Johklay <johnklayverton@gmail.com>
2026-04-06 16:52:24 -0300

    update

---
+++ aula053/script.js
@@ -0,0 +1,13 @@
+// IIFE -> immediately invoked function expression
+
+// (function () {
+//     console.log(1234567)
+// })();
+
+(function() {
+    const nome = 'Luiz';
+
+    console.log(nome);
+})();
+
+ const nome = 'qualquerCoisa';
