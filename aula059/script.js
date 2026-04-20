5e7fdb17aebd36afebcb02cb2cfc1768cd114f09
Johklay <johnklayverton@gmail.com>
2026-04-20 15:50:06 -0300

    update

---
+++ aula059/script.js
@@ -0,0 +1,75 @@
+function* geradora1() {
+    //codigo
+    yield 'valor 1'
+    //codigo
+    yield 'valor 2'
+    //codigo
+    yield 'valor 3'
+}
+
+// for (let valor of g1) {
+//     console.log(valor)
+// }
+
+// console.log(g1.next().value);
+// console.log(g1.next().value);
+// console.log(g1.next());
+// console.log(g1.next());
+
+function* geradora2(){
+    let i = 0;
+
+    while(true) {
+        yield i
+        i ++;
+    }
+}
+
+function* geradora3() {
+    yield  1;
+    yield  2;
+    yield  3;
+}
+
+function* geradora4() {
+    yield* geradora3();
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4();
+
+function* geradora5() {
+    yield function() {
+        console.log('vim do y1');
+    }
+
+    yield function() {
+        console.log('Vim do return')
+    }
+
+    yield function() {
+        console.log('vim do y3')
+    }
+}
+
+const g5 = geradora5();
+const func1 = g5.next().value
+const func2 = g5.next().value
+const func3 = g5.next().value
+
+func1();
+func2();
+func3();
+
+// for(let valor of g4) {
+//     console.log(valor)
+// }
+
+// const g2 = geradora2();
+// console.log(g2.next().value)
+
+
+
+
