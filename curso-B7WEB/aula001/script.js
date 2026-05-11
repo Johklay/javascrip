07e107035a2644c53014cf5e37bd8f9e0cd5db36
Johklay <johnklayverton@gmail.com>
2026-05-11 18:51:36 -0300

    Update script.js

--- curso-B7WEB/aula001/script.js
+++ curso-B7WEB/aula001/script.js
@@ -2,17 +2,37 @@
 //Programação Procedual
 // Programação funcional (PF - FP(Funcional programming paradigm))
 
+
 class Person {
-    constructor (name, age) {
+
+    age = 0;
+    steps = 0;
+
+    constructor (name) {
         this.name = name;
-        this.age = age;
     }
+
+    takeAstep() {
+        this.steps++;
+    }
+
+    setAge(newAge) {
+        if(typeof newAge === 'number'){
+            this.age = newAge;
+        }else {
+            console.log('Coloque apenas números')
+        }
+    }
+
 }
 
 let p1 = new Person("João", 20);
 let p2 = new Person("Maria", 30);
 let p3 = new Person("Pedro", 40);
 
-console.log(`${p1.name} tem ${p1.age} anos`);
-console.log(`${p2.name} tem ${p2.age} anos`);
-console.log(`${p3.name} tem ${p3.age} anos`);
+p1.setAge(20)
+p1.takeAstep();
+
+console.log(`Passos de ${p1.name}: ${p1.steps}`)
+console.log(`${p1.name} tem ${p1.age} anos`)
+console.log(`Passos de ${p2.name}: ${p2.steps}`)
