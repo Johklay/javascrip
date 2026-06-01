fe7756b4610b24839c9f37932d9909806e4ed95b
Johklay <johnklayverton@gmail.com>
2026-06-01 18:11:16 -0300

    update

---
+++ curso-B7WEB/aula-requisicoes/promises-async/script.js
@@ -0,0 +1,35 @@
+//https://jsonplaceholder.typicode.com/posts
+
+// async-await
+//async function clicou() {}
+//let clicou = async () => {}
+
+ async function clicou() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json();
+
+    alert(`Título do primeiro posts: ${json[0].title}`);
+    
+    alert("Clicou!");
+}
+
+async function inserir() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: 'Titulo de teste',
+            body: 'Corpo de teste',
+            userId: 2
+        })
+    });
+    let json = await response.json();
+
+    console.log(json);
+}
+
+document.querySelector('#botao').addEventListener('click', clicou);
+
+document.querySelector('#inserir').addEventListener('click', inserir);
