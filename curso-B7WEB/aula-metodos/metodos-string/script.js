9aa0a89ca7f5aaeb88a57bc88700ef2cb0d8ec67
Johklay <johnklayverton@gmail.com>
2026-06-03 17:17:55 -0300

    update

---
+++ curso-B7WEB/aula-metodos/metodos-string/script.js
@@ -0,0 +1,109 @@
+// INDEX OF
+
+// let nome = 'Bonieky Lacerda Leal';
+// let resultado = '';
+
+// let resultado = nome.indexOf('Lacerda');
+
+// if(nome.indexOf('Leal') > -1) {
+//     resultado = 'Achou!';
+// }else{
+//     resultado = 'não achou'
+// }
+
+// console.log(resultado);
+
+/////////////////////////////////////////////
+
+//SLICE
+
+// let nome = 'Bonieky Lacerda Leal';
+
+// let resultado = nome.slice(0, 10);
+
+// console.log(resultado);
+
+/////////////////////////////////////////////
+
+//SUBSTRING
+
+// let nome = 'Bonieky Lacerda Leal';
+
+// let resultado = nome.substring(0, 10);
+
+// console.log(resultado);
+
+////////////////////////////////////////////
+
+//SUBSTR 
+
+// let nome = 'Bonieky Lacerda Leal';
+
+// let resultado = nome.substr(8,15);
+
+// console.log(resultado);
+
+/////////////////////////////////////////////
+
+//REPLACE
+
+// let nome = 'Bonieky Lacerda Leal';
+
+// let resultado = nome.replace('Lacerda', 'Silva')
+
+// console.log('nome:', nome);
+// console.log("resultados:", resultado)
+
+//////////////////////////////////////////////
+
+//toUpperCase - letras maiuscalas
+// toLowerCase - letras minusculas 
+
+// let nome = 'Bonieky Lacerda Leal';
+
+// let resultado = nome.toUpperCase();
+
+// console.log("resultados:", resultado)
+
+////////////////////////////////////////////
+
+//CONCAT
+
+// let nome = 'Bonieky';
+
+// let resultado = nome.concat('Lacerda');
+
+// console.log("resultados:", resultado)
+
+////////////////////////////////////////////
+
+//TRIM
+
+// let nome = '        Bonieky       ';
+
+// let resultado = nome.trim();
+
+// console.log(resultado)
+
+///////////////////////////////////////////
+
+//charAt
+
+// let nome = 'Bonieky';
+
+// let resultado = nome.charAt(3);
+
+// console.log(resultado)
+
+//////////////////////////////////////////
+
+//SPLIT
+
+let nome = 'Bonieky Lacerda Leal';
+
+let resultado = nome.split(' ');
+
+console.log(resultado)
+
+
+
