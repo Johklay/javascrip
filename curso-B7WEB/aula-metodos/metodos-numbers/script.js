ad0747bf6697396ca0b262b3d26dbed17e7bd804
Johklay <johnklayverton@gmail.com>
2026-06-16 16:58:31 -0300

    update

---
+++ curso-B7WEB/aula-metodos/metodos-numbers/script.js
@@ -0,0 +1,32 @@
+//ToSTring
+
+// let n= 10;
+
+// let res = n.toString();
+
+// console.log(res);
+
+
+//ToFixed
+// let n= 10.838484;
+
+// let res = n.toFixed(2);
+
+// console.log(res);
+
+//parseInt
+
+// let n= '25';
+
+// let res = parseInt(n) + 5;
+
+// console.log(res);
+
+
+//parseFloat
+
+let n= '25.9';
+
+let res = parseFloat(n) + 5;
+
+console.log(res);
