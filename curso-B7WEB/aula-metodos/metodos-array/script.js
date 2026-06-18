eb3080f0a480bf5fd88d17731776ac8a85f4193e
Johklay <johnklayverton@gmail.com>
2026-06-18 16:42:26 -0300

    update

---
+++ curso-B7WEB/aula-metodos/metodos-array/script.js
@@ -0,0 +1,119 @@
+// Junta os valores do array separados por virgula em string
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// let res = lista.toString();
+
+// console.log(res);
+
+
+// Transforma o array em string, separando os elementos pelo item que selecionou (' - ')
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// let res = lista.join('-');
+
+// console.log(res);
+
+// Procura um item especifico no array e ele diz onde está 
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// let res = lista.indexOf('Corante');
+
+// console.log(res);
+
+
+//Remove o ultimo item do array
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista.pop();
+
+// let res = lista;
+
+// console.log(res);
+
+//Remove o primeiro item do array
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista.shift();
+
+// let res = lista;
+
+// console.log(res);
+
+//Adiciona novo item ao array
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista.push('açucar');
+
+// let res = lista;
+
+// console.log(res);
+
+
+// Altera/cria e substitui o item utilizando o indice do item
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista[0] = 'Ovos' ;
+
+// let res = lista;
+
+// console.log(res);
+
+//Mostra a quantidade de item no seu array utilizando lenght
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista[lista.length] = 'Ovos' ;
+
+// let res = lista;
+
+// console.log(res);
+
+//********************************************************************
+
+
+//Remover item do array utilizando splice
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista.splice(1, 1);
+
+// let res = lista;
+
+// console.log(res);
+
+//Junta arrays um com o outro/ concatena arrays
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+// let lista2 = ['Forma', 'Forno', 'Liquidificador', 'Prato']
+
+// let res = lista.concat(lista2);
+
+// console.log(res);
+
+//Ordena o array em ordem alfabetica 
+
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+// lista.sort();
+
+// let res = lista;
+
+// console.log(res);
+
+
+// Ordena o array em ordem descrecente, ao contrario
+let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+
+lista.sort();
+lista.reverse();
+
+let res = lista;
+
+console.log(res);
