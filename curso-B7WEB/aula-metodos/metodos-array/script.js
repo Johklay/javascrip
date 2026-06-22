4f21bbba77ea14bcbdfb5d5660ce61ab320e6b48
Johklay <johnklayverton@gmail.com>
2026-06-22 16:28:07 -0300

    Update script.js

--- curso-B7WEB/aula-metodos/metodos-array/script.js
+++ curso-B7WEB/aula-metodos/metodos-array/script.js
@@ -109,11 +109,116 @@
 
 
 // Ordena o array em ordem descrecente, ao contrario
-let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
+// let lista = ['Ovo', 'Farinha', 'Corante', 'Leite']
 
-lista.sort();
-lista.reverse();
+// lista.sort();
+// lista.reverse();
 
-let res = lista;
+// let res = lista;
+
+// console.log(res);
+
+//----------------------------------------------------------------------------------------------------
+
+//Mapeia o array e gera um novo array
+
+// let lista = [45, 4, 9, 16, 25];
+// let lista2 = [];
+
+// lista2 = lista.map(function (item) {
+//     return item * 2;
+// })
+
+// // for (let i in lista) {
+// //     lista2.push(lista[i] * 2);
+// // }
+ 
+// let res = lista2;
+
+// console.log(res);
+
+// Vai rodar a função e vai retornar false ou true
+
+// let lista = [45, 4, 9, 16, 25];
+// let lista2 = [];
+
+// lista2 = lista.filter(function (item) {
+//     if(item < 20) {
+//         return true;
+//     } else{
+//         return false;
+//     }
+// });
+
+ 
+// let res = lista2;
+
+// console.log(res);
+
+// vai verificar se é true ou false mas não vai gerar um novo array, exemplo: se todos os itens forem maior que 10 vai retornar apenas true se nao retorna false
+
+// let lista = [45, 4, 9, 16, 25];
+// let lista2 = [];
+
+// lista2 = lista.every(function (item) {
+//     // if(item > 3) {
+//     //     return true;
+//     // }else{
+//     //     return false;
+//     // }
+
+//     return (item > 3) ? true : false;
+// });
+
+ 
+// let res = lista2;
+
+// console.log(res);
+
+
+//Vai procuarar algo especifico no array e vai retornar o primeiro item com as condições definidas por vc 
+
+// let lista = [45, 4, 9, 16, 25];
+// let lista2 = [];
+
+// lista2 = lista.find(function (item) {
+//     return (item == 16) ? true : false;
+
+// });
+
+ 
+// let res = lista2;
+
+// console.log(res);
+
+// Retorna o index\Posição do item
+
+// let lista = [45, 4, 9, 16, 25];
+// let lista2 = [];
+
+// lista2 = lista.findIndex(function (item) {
+//     return (item == 16) ? true : false;
+
+// });
+
+ 
+// let res = lista2;
+
+// console.log(res);
+
+// exemplo real 
+
+let lista = [
+    {id: 1, nome:'Bonieky', sobrenome:'Lacerda'},
+    {id: 2, nome:'John', sobrenome:'Klayverton'},
+    {id: 3, nome:'J.K', sobrenome:'Sker'}
+];
+
+let pessoa = lista.find(function(item){
+    return (item.sobrenome == 'Sker') ? true : false;
+});
+
+ 
+let res = pessoa;
 
 console.log(res);
