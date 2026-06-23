e7e01cae3fb7ae21f7acdd3ca0aad8e1aa8b0f06
Johklay <johnklayverton@gmail.com>
2026-06-23 18:32:47 -0300

    update

---
+++ curso-B7WEB/datas/script.js
@@ -0,0 +1,13 @@
+let d = new Date();
+//SETANDO DATAS 
+
+//setFullyear - mudar ano
+//setMonth - mudar mês
+//setDate - mudar dia 
+//setDate( d.getDate() + 5) - para aumentar mais dias
+//setHours
+//setHours(d.getHours() + 3)- para aumentar as horas
+d.setFullYear(2027);
+
+let novoValor = d;
+console.log(novoValor)
