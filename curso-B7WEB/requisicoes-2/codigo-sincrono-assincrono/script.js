3a568efa334f640c5184f3ea2f68600c50ae3da8
Johklay <johnklayverton@gmail.com>
2026-07-08 18:54:47 -0300

    update

---
+++ curso-B7WEB/requisicoes-2/codigo-sincrono-assincrono/script.js
@@ -0,0 +1,16 @@
+//CODIGO SÍNCRONO
+
+let nome = 'John';
+let sobrenome = 'Klayverton';
+let completo = nome+ ' ' +sobrenome;
+
+console.log(completo)
+
+//CODIGO ASSÌNCRONO
+
+let nome = 'John';
+let sobrenome = 'Klayverton';
+let temperatura = Maquininha.pegarTemperatura();
+let completo = nome+ ' ' +sobrenome;
+
+console.log(completo)
---
+++ curso-B7WEB/requisicoes-2/funcao-callback/script.js
@@ -0,0 +1,10 @@
+//CALLBACK
+
+function alertar () {
+    console.log('Opa, tudo bem?');
+}
+
+let nome = 'John';
+setTimeout(alertar, 2000);
+let sobrenome = 'Klayverton';
+console.log("NOME COMPLETO =  "+nome+' '+sobrenome);
